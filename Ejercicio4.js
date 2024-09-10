f14fd09b4f40692561c0437532d307acc5e3ce91
LeonardoBustosVallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2024-09-10 12:45:21 -0300

    Ejercicio4.js
    - Condicionales

---
+++ Ejercicio4.js
@@ -0,0 +1,69 @@
+const numero1 = 10;
+const numero2 = 20;
+const numero3 = 30;
+
+// Imprimir en consola el número mayor de los tres.
+if (numero1 > numero2) {
+    a = numero1;
+} else {
+    a = numero2;
+}
+if (a > numero3) {
+    console.log(`El numero mayor es: ${a}`);
+} else {
+    console.log(`El numero mayor es: ${numero3}`);
+}
+
+// Imprimir en consola el número menor de los tres.
+if (numero1 < numero2) {
+    a = numero1;
+} else {
+    a = numero2;
+}
+if (a < numero3) {
+    console.log(`El numero menor es: ${a}`);
+} else {
+    console.log(`El numero menor es: ${numero3}`);
+}
+
+//Imprimir en consola si el numero1 es par o impar.
+if (numero1 % 2 === 0) {
+    console.log(`El numero1 (${numero1}) es par`);
+} else {
+    console.log(`El numero1 (${numero1}) es impar`);
+}
+
+//Imprimir en consola si el numero2 es par o impar.
+if (numero2 % 2 === 0) {
+    console.log(`El numero2 (${numero2}) es par`);
+} else {
+    console.log(`El numero2 (${numero2}) es impar`);
+}
+
+//Imprimir en consola si el numero3 es par o impar.
+if (numero3 % 2 === 0) {
+    console.log(`El numero3 (${numero3}) es par`);
+} else {
+    console.log(`El numero3 (${numero3}) es impar`);
+}
+
+//Imprimir en consola si el numero1 es múltiplo de 5.
+if (numero1 % 5 === 0) {
+    console.log(`El numero1 (${numero1}) es múltiplo de 5`);
+} else {
+    console.log(`El numero1 (${numero1}) no es múltiplo de 5`);
+}
+
+//Imprimir en consola si el numero2 es múltiplo de 5.
+if (numero2 % 5 === 0) {
+    console.log(`El numero2 (${numero2}) es múltiplo de 5`);
+} else {
+    console.log(`El numero2 (${numero2}) no es múltiplo de 5`);
+}
+
+//Imprimir en consola si el numero3 es múltiplo de 5.
+if (numero3 % 5 === 0) {
+    console.log(`El numero3 (${numero3}) es múltiplo de 5`);
+} else {
+    console.log(`El numero3 (${numero3}) no es múltiplo de 5`);
+}
