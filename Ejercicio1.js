95efbb8a71a11ae79ec0da38497cda381ac414c5
LeonardoBustosVallejos <103396502+LeonardoBustosVallejos@users.noreply.github.com>
2024-09-10 12:26:52 -0300

    add: Ejercicio1
    - Variables y tipos de datos
    - Strings, numbers y boolean

---
+++ Ejercicio1.js
@@ -0,0 +1,9 @@
+const nombre = "Leonardo";
+console.log(nombre);
+console.log(typeof (nombre));
+const edad = 27;
+console.log(edad);
+console.log(typeof (edad));
+const puedoConducir = true;
+console.log(puedoConducir);
+console.log(typeof (puedoConducir));
